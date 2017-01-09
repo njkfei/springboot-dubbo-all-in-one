eba10622a30a5598b2a07a10e150e24c25d7b71a
jpnie <jpnie@rxhui.com>
2017-01-09 15:03:44 +0800

    add dubbo trace

--- springbootprovideruser/src/main/java/com/jinhui/dubbo/serviceImpl/UserServiceImpl.java
+++ springbootprovideruser/src/main/java/com/jinhui/dubbo/serviceImpl/UserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Map;
 /**
  * Created by niejinping on 2016/12/23.
  */
-@Service(version="1.0.0")
+@Service(version="1.0.0",filter="TraceProviderFilter")
 public class UserServiceImpl implements UserService {
     private Map<Integer,User> userList = new HashMap<Integer,User>();
 
@@ -27,5 +27,13 @@ public class UserServiceImpl implements UserService {
         User user = new User(userId,userName);
         userList.put(userId,user);
         return 1;
+    }
+
+    public String getUserName(int userId){
+        User user = userList.get(userId);
+        if(user == null){
+            return new User(0,"nouserid").getUserName();
+        }
+        return user.getUserName();
     }
 }
